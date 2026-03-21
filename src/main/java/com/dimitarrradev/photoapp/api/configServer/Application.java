fbb3c2dab8845c160dab28e0219e8f5d769eb21e
DimitarRadev88 <dimitar.rumenov.radev@gmail.com>
2026-03-21 10:12:12 +0200

    Add: Spring Security dependency and filterChain configuration with basic authentication

--- src/main/java/com/dimitarrradev/photoapp/api/configServer/Application.java
+++ src/main/java/com/dimitarrradev/photoapp/api/configServer/Application.java
@@ -3,6 +3,9 @@ package com.dimitarrradev.photoapp.api.configServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -10,6 +13,11 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
---
+++ src/main/java/com/dimitarrradev/photoapp/api/configServer/security/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.dimitarrradev.photoapp.api.configServer.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
+    @Value("${spring.security.user.name}")
+    private String adminUsername;
+    @Value("${spring.security.user.password}")
+    private String adminPassword;
+    @Value("${spring.security.user.roles}")
+    private String roleAdmin;
+
+    @Value("${custom.security.user.name}")
+    private String clientUsername;
+    @Value("${custom.security.user.password}")
+    private String clientPassword;
+    @Value("${custom.security.user.roles}")
+    private String clientRole;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) {
+
+        http.authorizeHttpRequests(registry -> {
+                    registry
+                            .requestMatchers(HttpMethod.POST, "/actuator/busrefresh").hasRole("ADMIN")
+                            .requestMatchers(HttpMethod.GET, "/**").hasRole("CLIENT")
+                            .anyRequest().authenticated();
+                }).csrf(csrf -> csrf.ignoringRequestMatchers("/actuator/busrefresh"))
+                .cors(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder) {
+        UserDetails admin = User.builder()
+                .username(adminUsername)
+                .password(passwordEncoder.encode(adminPassword))
+                .roles(roleAdmin)
+                .build();
+
+        UserDetails client = User.builder()
+                .username(clientUsername)
+                .password(passwordEncoder.encode(clientPassword))
+                .roles(clientRole)
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, client);
+    }
+}
